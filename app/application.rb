0b6b50aa3051eaa08ca7ebd95c95fad307840cb6
Christine Xiang <yutingcxiang@gmail.com>
2018-08-13 01:14:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,7 +13,7 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    elsif req.path.match(/cart)
+    elsif req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}\n"
       end
