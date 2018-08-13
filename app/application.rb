fac9782fb554b60c20e15063da97f3ef528b3e9f
Christine Xiang <yutingcxiang@gmail.com>
2018-08-13 01:19:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,7 @@ class Application
     elsif req.path.match(/add/)
       search_term = req.params["q"]
       if @@items.include?(search_term)
-        @@carts << search_term
+        @@cart << search_term
       else
         return "We don't have that item"
       end
