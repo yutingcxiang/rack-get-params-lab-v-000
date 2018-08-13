e94cbe8437875c668c5bbb63686cba4c71dbc324
Christine Xiang <yutingcxiang@gmail.com>
2018-08-13 01:28:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -22,7 +22,7 @@ class Application
         end
       end
     elsif req.path.match(/add/)
-      search_term = req.params["items"]
+      search_term = req.params["item"]
       if @@items.include?(search_term)
         @@cart << search_term
         resp.write "added #{search_term}"
